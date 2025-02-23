c37293c039061aa5adee00a87cb3a47b8de83042
Sebastian Ciaglo <sebastian.ciaglo@gmail.com>
2025-02-23 18:55:39 +0100

    Homework day 5

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int number = 4522;
+
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
+int[] howMuch = new int[10];
+
+foreach (char c in letters)
+{
+      for (int i = 0; i < 10; i++)
+    {
+        int toInt = c - '0';
+        if (toInt ==i)
+        {
+            howMuch[i] = howMuch[i] + 1;
+        }
+
+    }
+
+ 
+}
+
+Console.WriteLine("Wyniki dla liczby: " + number);
+
+for (int i = 0;i < howMuch.Length; i++)
+{
+    Console.WriteLine(i + "=>" + howMuch[i]);
+}
