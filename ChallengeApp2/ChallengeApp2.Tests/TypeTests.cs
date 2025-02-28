6faf76b9ea8c3a9b43b653c6f9d86f32930be703
Sebastian Ciaglo <sebastian.ciaglo@gmail.com>
2025-02-28 01:23:48 +0100

    Day 8 homework

---
+++ ChallengeApp2/ChallengeApp2.Tests/TypeTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ChallengeApp2.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void GetEmployeeShouldReturnSameValues()
+        {
+            // arrange
+
+            int number1 = 1;
+            int number2 = 1;
+
+            // act
+
+
+            // assert
+
+            Assert.AreEqual(number1, number2);
+
+        }
+
+
+
+        [Test]
+        public void GetEmployeeShouldReturnDifferentObjects()
+        {
+            // arrange
+
+            var employee1 = GetEmployee("Janusz", "T", 66);
+            var employee2 = GetEmployee("Jan", "Z", 52);
+
+            // act
+
+
+            // assert
+
+            Assert.AreNotEqual(employee1, employee2);
+
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+
+            return new Employee(name, surname, age);
+
+        }
+
+
+    }
+}
